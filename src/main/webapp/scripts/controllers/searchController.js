bbf728cb2c36bc3c4b2957a0356bf841a194b16b
ifcunharro <ifcunharro@gmail.com>
2014-04-22 21:00:10 +0200

    Arreglado lo que comentabas del espacio que aparecía en el select. Se puso valor por defecto Categoría para evitar tener que seleccionar la Opción Todo para realizar la primera búsqueda pulsando enter. Ahora aunque no selecciones Todo, la búsqueda se hará en todo y así se mostrará Cambiando la palabra Categoría por Todo.
    Se borró el código comentado y se añadió un evento ng-change para
    sustituir el ng-click que teníamos para evitar que al hacer click en el
    desplegable se haga una búsqueda cuando ponga cualquier cosa que no sea
    Categoría.

--- src/main/webapp/scripts/controllers/searchController.js
+++ src/main/webapp/scripts/controllers/searchController.js
@@ -8,23 +8,26 @@ define(['controllers/controllers'], function(controllers) {
             $scope.terms = '';
 
             $scope.categories = [
-                { name: 'Todo',    path: ''  },
+                { name: 'Categoría', path: ''},
+                { name: 'Todo',    	 path: 'articles'  },
                 { name: 'Libros',    path: 'books'  },
                 { name: 'Cómics',    path: 'comics' },
                 { name: 'Películas', path: 'movies' },
                 { name: 'Música',    path: 'music'  },
             ];
-
+            $scope.selectedCategory = $scope.categories[0];
+            
             $scope.search = function(category) {
                 $location.path("/" + category.path + "/search/" + $scope.terms);
             };
-            
+
             $scope.enter = function(ev) {
             	 if (ev.which==13){
-            		 if ($scope.selectedCategory.name != 'Todo')
+            		 if ($scope.selectedCategory.name != 'Categoría')
             			 $location.path("/" + $scope.selectedCategory.path + "/search/" + $scope.terms);
             		 else
             			 $location.path("/" + 'articles'+ "/search/" + $scope.terms);
+            		 	 $scope.selectedCategory = $scope.categories[1];
             	 }
             };
 
