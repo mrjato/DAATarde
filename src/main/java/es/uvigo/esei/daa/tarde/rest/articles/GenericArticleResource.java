ec519e0677e1e493b3b3fb5fb608f1f9b0f86fd6
Alberto G. Jácome <agjacome@gmail.com>
2014-04-25 18:03:04 +0200

    Añadido contador de resultados de busqueda a API REST
    Se ha añadido un QueryParam a la API REST para poder devolver el numero de
    resultados de una busqueda por nombre. Así, por ejemplo, para buscar por titulo
    "mytitle" en todos los articulos, existen las siguientes URLs (todas via HTTP
    GET):
    
        - "/rest/articles?search=mytitle"            : primera pagina
        - "/rest/articles?search=mytitle&page=6"     : pagina 6
        - "/rest/articles?search=mytitle&count=true" : numero de resultados
    
    Para obtener el numero de resultados por pagina basta con comprobar la longitud
    del listado de articulos en la primera pagina. Y para calcular el numero de
    paginas existentes bastaria con algo del estilo:
    
        Math.ceil(numeroDeResultados / longitudPrimeraPagina)

--- src/main/java/es/uvigo/esei/daa/tarde/rest/articles/GenericArticleResource.java
+++ src/main/java/es/uvigo/esei/daa/tarde/rest/articles/GenericArticleResource.java
@@ -24,32 +24,77 @@ public abstract class GenericArticleResource<T extends Article> {
         this.dao = dao;
     }
 
+    /*
+     * Supongase @Path("/articles") en la clase concreta, entonces:
+     *
+     * - HTTP GET a "/articles" retorna primera pagina del listado
+     *   completo de articulos (realiza busqueda vacia).
+     *
+     * - HTTP GET a "/articles?page=7" retorna la pagina 7 del
+     *   listado completo de articulos (reliza busqueda vacia).
+     *
+     * - HTTP GET a "/articles?search=xxx" retorna primera pagina del
+     *   listado de articulos coincidente con busqueda por nombre
+     *   "xxx".
+     *
+     * - HTTP GET a "/articles?search=xxx&page=7" retorna la pagina 7
+     *   del listado de articulos coincidentes con busqueda por
+     *   nombre "xxx".
+     *
+     * - HTTP GET a "/articles?count=true" retorna numero absoluto de
+     *   articulos existentes (realiza COUNT sobre busqueda vacia).
+     *
+     * - HTTP GET a "/articles?search=xxx&count=true" retorna numero
+     *   de articulos coincidentes con busqueda por nombre "xxx"
+     *   (realiza COUNT sobre busqueda por nombre "xxx").
+     *
+     */
     @GET
-    @SuppressWarnings("unchecked")
-    public Response search(
-        @QueryParam("search") final String name,
-        @QueryParam("page") @DefaultValue("1") final int pageNumber
+    public Response list(
+        @QueryParam("search") @DefaultValue("")      final String name,
+        @QueryParam("count")  @DefaultValue("false") final boolean count,
+        @QueryParam("page")   @DefaultValue("1")     final int pageNumber
     ) {
         try {
-            return Response.ok().entity(new GenericEntity<List<Article>>(
-                (List<Article>) dao.findByName(
-                    name, pageNumber, Config.getInteger("articles_per_page")
-                )
-            ) { }).build();
-        } catch (final Exception _) {
+
+            return Response.ok().entity(
+                count ? countByName(name) : findByName(name, pageNumber)
+            ).build();
+
+        } catch (final IllegalArgumentException iae) {
+            iae.printStackTrace();
+            return Response.status(Status.BAD_REQUEST).build();
+        } catch (final Exception e) {
+            e.printStackTrace();
             return Response.serverError().build();
         }
     }
 
+    protected GenericEntity<Long> countByName(final String name) {
+        return new GenericEntity<Long>(dao.countByName(name)) { };
+    }
+
+    @SuppressWarnings("unchecked")
+    protected GenericEntity<List<Article>> findByName(
+        final String name, final int pageNumber
+    ) {
+        return new GenericEntity<List<Article>>((List<Article>) dao.findByName(
+            name, pageNumber, Config.getInteger("articles_per_page")
+        )) { };
+    }
+
     @GET
     @Path("latest")
+    @SuppressWarnings("unchecked")
     public Response findLatest() {
         try {
+
             return Response.ok().entity(new GenericEntity<List<Article>>(
                 (List<Article>) dao.findLatest(
                     Config.getInteger("articles_home_page")
                 )
             ) { }).build();
+
         } catch (final Exception _) {
             return Response.serverError().build();
         }
@@ -57,12 +102,14 @@ public abstract class GenericArticleResource<T extends Article> {
 
     @POST
     public Response insert(final T article) {
-        if (article.getId() != null)
+        if (article.isPersisted())
             return Response.status(Status.BAD_REQUEST).build();
 
         try {
+
             dao.save(article);
             return Response.ok(article.getId()).build();
+
         } catch (final PersistenceException pe) {
             pe.printStackTrace();
             return Response.serverError().build();
