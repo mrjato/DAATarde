d85d7279292855522ab26415e626a1b1997b7791
ifcunharro <ifcunharro@gmail.com>
2014-05-05 14:16:00 +0200

    Arreglado listar, aun no pagina pero ya filtra

--- src/main/webapp/scripts/controllers/searchController.js
+++ src/main/webapp/scripts/controllers/searchController.js
@@ -8,25 +8,27 @@ define(['controllers/controllers'], function(controllers) {
             $scope.terms = '';
 
             $scope.categories = [
-                { name: 'Todo',    	 path: 'articles'  },
-                { name: 'Libros',    path: 'books'  },
-                { name: 'Cómics',    path: 'comics' },
+                { name: 'Todo', path: 'articles' },
+                { name: 'Libros', path: 'books' },
+                { name: 'Cómics', path: 'comics' },
                 { name: 'Películas', path: 'movies' },
-                { name: 'Música',    path: 'music'  },
+                { name: 'Música', path: 'music' },
             ];
             $scope.selectedCategory = $scope.categories[0];
             
             $scope.search = function(category) {
-                $location.path("/" + category.path + "?search=" + $scope.terms);
+                $location.path("/" + category.path + "/search/" + $scope.terms);
             };
 
             $scope.enter = function(ev) {
-            	 if (ev.which==13){
-            		 if ($scope.selectedCategory.name == 'Todo') {
-            			 $scope.selectedCategory = $scope.categories[0];
-            		 }
-            	 $location.path("/" + $scope.selectedCategory.path + "?search=" + $scope.terms);
+             if (ev.which==13){
+            	 if ($scope.selectedCategory.name != 'Todo')
+            		 $location.path("/" + $scope.selectedCategory.path + "/search/" + $scope.terms);
+            	 else{
+            		 $location.path("/" + 'articles'+ "/search/" + $scope.terms);
+            		
             	 }
+             }
             };
 
         }]
